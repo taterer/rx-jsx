3b7e9022b49b725ee22e8c6a333affaf65082573
Taylor McKay <jtmckay@gmail.com>
2023-02-15 16:35:51 -0700

    Rename export to jsx

--- package/README.md
+++ package/README.md
@@ -1,28 +1,35 @@
 # Rx-JSX
+
 A familiar way to develop a front end using JSX, but without React. Prop drilling and lifecycle events are all replaced with simply RxJS.
 
 ## Why use RxJSX?
+
 You prefer functional (reactive) programming, you'd rather not learn more abstractions on top of html to change styles, classes, or add event listeners. Minimalist code, with no magic: it does exactly what you expect it to do.
 
 ## Getting Started
+
 ### Install
-```yarn add @taterer/rx-jsx rxjs```
+
+`yarn add @taterer/rx-jsx rxjs`
 
 or
 
- ```npm i --save @taterer/rx-jsx rxjs```
+`npm i --save @taterer/rx-jsx rxjs`
 
 ### Setup
+
 .babelrc
+
 ```
 {
   "plugins": [
-    ["@babel/transform-react-jsx", { "pragma": "rxjsx" }]
+    ["@babel/transform-react-jsx", { "pragma": "jsx" }]
   ]
 }
 ```
 
 ### Import
+
 ```
 import '@taterer/rx-jsx' // this must be imported before any JSX is rendered
 import App from './App'
@@ -31,11 +38,15 @@ document.getElementById('root')?.appendChild(<App />);
 ```
 
 ### Define
+
 This is enough to render JSX at any depth. App is expected to be a function that returns JSX. EG:
+
 ```
 const App = () => <div />
 ```
+
 The element can be defined with styles, class, events, or with children. EG:
+
 ```
 const App = () => <div>
   <Route />
@@ -44,10 +55,13 @@ const App = () => <div>
 ```
 
 ## How to use
+
 A full example codebase with exercises for training can be found at: https://github.com/taterer/rx-jsx
 
 ### single$
+
 Define single$ on an html element. When the JSX element is created it will automatically subscribe to the observable passed to single$. The observable should emit HTML elements. EG:
+
 ```
 import { css } from '@emotion/css'
 import { of } from 'rxjs'
@@ -67,14 +81,17 @@ export default function App () {
   )
 }
 ```
+
 Using single$ will replace the HTML element with the latest emission. When single$ or multi$ has a pipe using takeUntil, it will remove the root element upon completion.
 
 ### multi$
+
 The multi$ property does the exact same thing as single$, except that it is append only. The previous element will not be removed with a new emission of the observable.
 
 ### Best practices
+
 - Name observables/subjects using the $ suffix.
 - Always use a destruction$ observable in a takeUntil as the last operator in pipes before passing them to single$, multi$, or creating any subscriptions.
-- If things are not rendering properly, try withAnimationFrame. ```import { withAnimationFrame } from @taterer/rx-jsx```
+- If things are not rendering properly, try withAnimationFrame. `import { withAnimationFrame } from @taterer/rx-jsx`
 - It can be useful to throw in a share() when setting up an observable for single$/multi$ so that other subscriptions end up with the same JSX element.
 - Use RxJS Debugger for tracking emissions https://www.npmjs.com/package/@taterer/rxjs-debugger
--- package/index.ts
+++ package/index.ts
@@ -8,54 +8,54 @@ import {
   scan,
   startWith,
   switchMap,
-} from 'rxjs';
+} from "rxjs";
 
-export type HTMLDestroyElement = HTMLElement & { destroy: Function }
+export type HTMLDestroyElement = HTMLElement & { destroy: Function };
 
 // Typescript complains when React is undefined while working with JSX,
 // even if you're not using it
 declare global {
-  const React: any
+  const React: any;
 }
 
 // Critical JSX replacement
 
-(window as any).rxjsx = (tag, props, ...children) => {
-  if (typeof tag === 'function') return tag(props, ...children);
+(window as any).jsx = (tag, props, ...children) => {
+  if (typeof tag === "function") return tag(props, ...children);
   const element = document.createElement(tag);
 
   Object.entries(props || {}).forEach(([name, value]: [string, any]) => {
-    if (name.startsWith('on') && name.toLowerCase() in window) {
+    if (name.startsWith("on") && name.toLowerCase() in window) {
       element.addEventListener(name.toLowerCase().substr(2), value);
-    } else if (name === 'single$') {
+    } else if (name === "single$") {
       const observable = value.pipe(
         scan((acc, current) => {
           if (acc) {
-            element.removeChild(acc)
+            element.removeChild(acc);
           }
           if (current) {
-            element.appendChild(current)
+            element.appendChild(current);
           }
-          return current
+          return current;
         }, undefined)
-      )
-      observable.subscribe({ complete: () => element.remove() })
-    } else if (name === 'multi$') {
+      );
+      observable.subscribe({ complete: () => element.remove() });
+    } else if (name === "multi$") {
       const observable = value.pipe(
-        map(current => {
+        map((current) => {
           if (current) {
-            element.appendChild(current)
+            element.appendChild(current);
           }
-          return current
+          return current;
         })
-      )
-      observable.subscribe({ complete: () => element.remove() })
+      );
+      observable.subscribe({ complete: () => element.remove() });
     } else {
       element.setAttribute(name, value.toString());
     }
   });
 
-  children.forEach(child => {
+  children.forEach((child) => {
     appendChild(element, child);
   });
 
@@ -64,40 +64,50 @@ declare global {
 
 const appendChild = (parent, child) => {
   if (Array.isArray(child))
-    child.forEach(nestedChild => appendChild(parent, nestedChild));
+    child.forEach((nestedChild) => appendChild(parent, nestedChild));
   else
     parent.appendChild(child.nodeType ? child : document.createTextNode(child));
 };
 
 // Helper functions
 
-export const withAnimationFrame: OperatorFunction<any, any> = switchMap(async (value) => {
-  return await new Promise(resolve => {
-    animationFrameScheduler.schedule(() => {
-      resolve(value)
-    })
-  })
-})
+export const withAnimationFrame: OperatorFunction<any, any> = switchMap(
+  async (value) => {
+    return await new Promise((resolve) => {
+      animationFrameScheduler.schedule(() => {
+        resolve(value);
+      });
+    });
+  }
+);
 
-export function fromEventElement$ (target$: Observable<Element>, eventName: string) {
-  return target$
-  .pipe(
-    switchMap(target => fromEvent(target, eventName))
-  )
+export function fromEventElement$(
+  target$: Observable<Element>,
+  eventName: string
+) {
+  return target$.pipe(switchMap((target) => fromEvent(target, eventName)));
 }
 
-export function fromValueElementKeyup$(target$: Observable<Element>, defaultValue?: string): Observable<string> {
-  return target$
-  .pipe(
-    switchMap<Element, Observable<string>>(target => fromEvent<any>(target, 'keyup').pipe(pluck('target', 'value'))),
-    startWith(defaultValue || ''),
-  )
+export function fromValueElementKeyup$(
+  target$: Observable<Element>,
+  defaultValue?: string
+): Observable<string> {
+  return target$.pipe(
+    switchMap<Element, Observable<string>>((target) =>
+      fromEvent<any>(target, "keyup").pipe(pluck("target", "value"))
+    ),
+    startWith(defaultValue || "")
+  );
 }
 
-export function classSync (target: Element, classToSync: string, shouldHaveClass: boolean): void {
+export function classSync(
+  target: Element,
+  classToSync: string,
+  shouldHaveClass: boolean
+): void {
   if (shouldHaveClass) {
-    target.classList.add(classToSync)
+    target.classList.add(classToSync);
   } else {
-    target.classList.remove(classToSync)
+    target.classList.remove(classToSync);
   }
 }
